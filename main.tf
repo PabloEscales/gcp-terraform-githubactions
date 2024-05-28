e179a9e534ee88545e9f7c26e6beed2ff2fe5a27
PabloEscales <pabloescales@gmail.com>
2024-05-28 17:11:31 +0200

    3

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "google_storage_bucket" {
+resource "google_storage_bucket" "my-bucket" {
   name                     = "poel-test-s3"
   location                 = "EU"
+  project                  = ""
   force_destroy            = true
   public_access_prevention = "enforced"
 }
