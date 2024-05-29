87cbe677e9c859c3e2a49990a0ee7527eac4108d
PabloEscales <pabloescales@gmail.com>
2024-05-29 10:21:10 +0200

    changed to privet bucket

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "poel-test-s3"
-  location      = "EU"
-  project       = "spa-newlearningdev-dev-001"
-  force_destroy = true
+  name                        = "poel-test-s3"
+  location                    = "EU"
+  project                     = "spa-newlearningdev-dev-001"
+  force_destroy               = true
+  uniform_bucket_level_access = true
 }
 
 # Make a new object public
-resource "google_storage_object_access_control" "public_rule" {
-  object = google_storage_bucket_object.static_site_src.name
+resource "google_storage_object_acl" "public_rule" {
   bucket = google_storage_bucket.my-bucket.name
+  object = google_storage_bucket_object.static_site_src.name
   role   = "READER"
   entity = "allUsers"
 }
