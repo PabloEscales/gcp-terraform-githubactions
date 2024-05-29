bd2c35d61ae06ea11bb8bb994257a877a1ab2a12
PabloEscales <pabloescales@gmail.com>
2024-05-29 10:05:46 +0200

    force_destroy removed

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "google_storage_bucket" "my-bucket" {
   location                 = "EU"
   project                  = "spa-newlearningdev-dev-001"
   force_destroy            = true
-  public_access_prevention = "enforced"
 }
 
 # Make a new object public
