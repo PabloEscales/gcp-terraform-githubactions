fe05af9f5836687584112e6d5f0cf3e1122c8850
PabloEscales <pabloescales@gmail.com>
2024-05-29 11:27:33 +0200

    upload object

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ resource "google_storage_bucket" "my-bucket" {
   uniform_bucket_level_access = true
 }
 
-# Make a new object public
-resource "google_storage_object_acl" "public_rule" {
-  bucket = google_storage_bucket.my-bucket.name
-  object = google_storage_bucket_object.static_site_src.name
-  role   = "READER"
-  entity = "allUsers"
-}
+# # Make a new object public
+# resource "google_storage_object_acl" "public_rule" {
+#   bucket = google_storage_bucket.my-bucket.name
+#   object = google_storage_bucket_object.static_site_src.name
+#   role   = "READER"
+#   entity = "allUsers"
+# }
 
 # Upload the index.html file to the bucket
 resource "google_storage_bucket_object" "static_site_src" {
