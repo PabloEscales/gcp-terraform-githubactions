a0e3fd6ebad614ec5fcb70fba91af9859f5b3e5b
PabloEscales <pabloescales@gmail.com>
2024-05-29 10:04:24 +0200

    one step less fixed

--- main.tf
+++ main.tf
@@ -17,6 +17,6 @@ resource "google_storage_object_access_control" "public_rule" {
 # Upload the index.html file to the bucket
 resource "google_storage_bucket_object" "static_site_src" {
   name   = "index.html"
-  source = "../website/index.html"
+  source = "./website/index.html"
   bucket = google_storage_bucket.my-bucket.name
 }
