a9fd277d17e17ecb088507ef1db850dae6633ddc
PabloEscales <pabloescales@gmail.com>
2024-05-29 11:52:16 +0200

    clean code

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ resource "google_storage_bucket" "my-bucket" {
   uniform_bucket_level_access = true
 }
 
-# # Make a new object public
-# resource "google_storage_object_acl" "public_rule" {
-#   bucket = google_storage_bucket.my-bucket.name
-#   object = google_storage_bucket_object.static_site_src.name
-#   role   = "READER"
-#   entity = "allUsers"
-# }
-
 resource "google_storage_bucket_iam_binding" "object_access_binding" {
   bucket = google_storage_bucket.my-bucket.name
   role   = "roles/storage.objectViewer"
@@ -33,17 +25,5 @@ resource "google_storage_bucket_iam_member" "bucket_access" {
 resource "google_storage_bucket_object" "static_site_src" {
   name   = "index.html"
   source = "./website/index.html"
-  bucket = google_storage_bucket.my-bucket.name
-}
-
-resource "google_storage_bucket_object" "google_logo_png" {
-  name   = "google-logo.png"
-  source = "./website/google-logo.png"
-  bucket = google_storage_bucket.my-bucket.name
-}
-
-resource "google_storage_bucket_object" "style_css" {
-  name   = "style.css"
-  source = "./website/style.css"
   bucket = google_storage_bucket.my-bucket.name
 }
