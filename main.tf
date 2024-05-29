6743f08806ee3dd576fc24387cf92f10b9ae9189
PabloEscales <pabloescales@gmail.com>
2024-05-29 09:56:30 +0200

    code formated

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ resource "google_storage_bucket" "my-bucket" {
 resource "google_storage_object_access_control" "public_rule" {
   object = google_storage_bucket_object.static_site_src.name
   bucket = google_storage_bucket.my-bucket.name
-  role = "READER"
+  role   = "READER"
   entity = "allUsers"
 }
 
 # Upload the index.html file to the bucket
 resource "google_storage_bucket_object" "static_site_src" {
-  name = "index.html"
+  name   = "index.html"
   source = "../website/index.html"
   bucket = google_storage_bucket.my-bucket.name
 }
